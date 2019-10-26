a2e3f06220dc74d9bbd817bfe457133dd4e0494b
Michael Kha <mxk5025@rit.edu>
2019-10-26 13:41:40 -0400

    Working on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/shared/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Header
+        account={{ first: "Mike", last: "K" }}
+      />
+    </div>
   );
 }
 
---
+++ src/components/shared/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header({account}) {
+
+  return (
+    <div className="Header">
+      
+    </div>
+  )
+
+}
