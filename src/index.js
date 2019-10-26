0cf4badc40b2f85f2b5cf3584f536feee7945747
Mark Vittozzi <mev5063@rit.edu>
2019-10-26 15:51:04 -0400

    Commiting what I have so I can merge master in

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Reserve from './Reserve'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Reserve />, document.getElementById('root'));
