6c43409d771a94c83cd766fa7d57f4de2f13fe28
Mark Vittozzi <mev5063@rit.edu>
2019-10-27 15:14:21 -0400

    Adding functionality to toggle between map and table view

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -4,8 +4,24 @@ import ReserveTable from './reserve-page/ReserveTable';
 import DateSelector from './reserve-page/DateSelector'
 import RightSideBar from './reserve-page/RightSideBar'
 import './Reserve.css'
+import _ from 'lodash';
 
 class Reserve extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMap: false
+    }
+
+    this.toggleView = this.toggleView.bind(this);
+  }
+
+  toggleView() {
+    var stateHolder = _.clone(this.state);
+    stateHolder.showMap = !stateHolder.showMap;
+    this.setState(stateHolder);
+  }
   render() {
     return (
     <div className="reserve-main">
@@ -14,10 +30,10 @@ class Reserve extends React.Component {
           <DateSelector/>
         </div>
         <div className="middle-reserve">
-          <ReserveTable numberRooms={10} showFloorButtons={true}/>
+          {_.get(this.state,'showMap',false) ? <ReserveMap/> : <ReserveTable numberRooms={10} showFloorButtons={true}/>}
         </div>
         <div className="right-side-reserve">
-            <RightSideBar/>
+            <RightSideBar toggleView={this.toggleView}/>
         </div>
     </div>
   )
